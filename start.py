6d2ad8ca8b81f2c8b7131388bd819ed936a8c30a
dajung929 <uirr1004@gmail.com>
2025-04-24 16:52:34 +0900

    html 파일 선언

--- start.py
+++ start.py
@@ -2,7 +2,7 @@
 # sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from time import sleep
 import unittest
-import HtmlTestRunner # type: ignore
+import HtmlTestRunner 
 
 
 
