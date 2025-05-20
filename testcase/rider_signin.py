b170cf154045e3849d749e5fe4348f001cf7d754
dajung929 <uirr1004@gmail.com>
2025-05-20 18:15:07 +0900

    import 추가

--- testcase/rider_signin.py
+++ testcase/rider_signin.py
@@ -1,5 +1,6 @@
 
 from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.common.by import By
 from appium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
